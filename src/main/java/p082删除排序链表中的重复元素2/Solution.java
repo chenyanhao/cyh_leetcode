008da7eb76838923a7fe53adbef54d59082b1b43
ha <ha@ha.local>
2020-07-15 08:53:03 +0800

    solve problem83

---
+++ src/main/java/p082删除排序链表中的重复元素2/Solution.java
@@ -0,0 +1,7 @@
+package p082删除排序链表中的重复元素2;
+
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        return null;
+    }
+}
---
+++ src/main/java/p083删除排序链表中的重复元素/Solution.java
@@ -0,0 +1,15 @@
+package p083删除排序链表中的重复元素;
+
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode cur = head;
+        while (cur != null && cur.next != null) {
+            if (cur.val == cur.next.val) {
+                cur.next = cur.next.next;
+            } else {
+                cur = cur.next;
+            }
+        }
+        return head;
+    }
+}
