1cfd33487a2b9cba2084a0e53da5eac2e002cef7
chenyanhao <chen_yanhao@126.com>
2020-07-20 20:16:49 +0800

    完成题目

--- src/main/java/p083删除排序链表中的重复元素/Solution.java
+++ src/main/java/p083删除排序链表中的重复元素/Solution.java
@@ -2,14 +2,24 @@ package p083删除排序链表中的重复元素;
 
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
-        ListNode cur = head;
-        while (cur != null && cur.next != null) {
-            if (cur.val == cur.next.val) {
-                cur.next = cur.next.next;
+        ListNode headfirst = new ListNode(-1);
+        headfirst.next = head;
+
+        ListNode a = headfirst, b  = head;
+        while (b != null && b.next != null) {
+            if (a.next.val == b.next.val) {
+                while (b.next != null && b.val == b.next.val) {
+                    b = b.next;
+                }
+                a.next = b.next;
+                b.next = null;
+                b = a.next;
             } else {
-                cur = cur.next;
+                a = a.next;
+                b = b.next;
             }
         }
-        return head;
+
+        return headfirst.next;
     }
 }
