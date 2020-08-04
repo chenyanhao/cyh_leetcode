3eb1cd4dd8b9841e40063a0e1a830b99d450ad7b
chenyanhao <chen_yanhao@126.com>
2020-08-04 09:06:04 +0800

    solve p101

---
+++ src/main/java/p101_200/p101对称二叉树/Solution.java
@@ -0,0 +1,20 @@
+package p101_200.p101对称二叉树;
+
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        return check(root, root);
+    }
+
+    private boolean check(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+
+        if (p == null || q == null) {
+            return false;
+        }
+
+        return p.val == q.val && check(p.left, q.right) && check(p.right, q.left);
+
+    }
+}
---
+++ src/main/java/p101_200/p101对称二叉树/TreeNode.java
@@ -0,0 +1,8 @@
+package p101_200.p101对称二叉树;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
