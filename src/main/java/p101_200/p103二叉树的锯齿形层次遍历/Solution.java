3fbe6ee87e0dc4580b5902d35928241920187258
chenyanhao <chen_yanhao@126.com>
2020-08-06 11:23:50 +0800

    solve zigzag

--- src/main/java/p101_200/p103二叉树的锯齿形层次遍历/Solution.java
+++ src/main/java/p101_200/p103二叉树的锯齿形层次遍历/Solution.java
@@ -1,14 +1,52 @@
 package p101_200.p103二叉树的锯齿形层次遍历;
 
 import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
 import java.util.List;
 
 class Solution {
+
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
         List<List<Integer>> res = new ArrayList<>();
         if (root == null) {
             return res;
         }
+
+        //双端队列，两边都可以操作
+        Deque<TreeNode> deque = new LinkedList<>();
+        //添加到队列的头
+        deque.addFirst(root);
+        while (!deque.isEmpty()) {
+            List<Integer> level = new ArrayList<>();
+            //统计这一行有多少个节点
+            int count = deque.size();
+            //遍历这一行的所有节点
+            TreeNode cur;
+            for (int i = 0; i < count; i++) {
+                if (res.size() % 2 == 1) {
+                    //从左边往右边打印
+                    //移除队列头部的元素，如果子节点不为空加入到队列的尾部
+                    cur = deque.removeFirst();
+                    if (cur.right != null)
+                        deque.addLast(cur.right);
+                    if (cur.left != null)
+                        deque.addLast(cur.left);
+                } else {
+                    //从右边往左边打印
+                    //移除队列尾部的元素，如果子节点不为空加入到队列的头部
+                    cur = deque.removeLast();
+                    if (cur.left != null)
+                        deque.addFirst(cur.left);
+                    if (cur.right != null)
+                        deque.addFirst(cur.right);
+                }
+                level.add(cur.val);
+            }
+            res.add(level);
+        }
         return res;
     }
+
+
 }
---
+++ src/main/java/p101_200/p103二叉树的锯齿形层次遍历/Solution2.java
@@ -0,0 +1,43 @@
+package p101_200.p103二叉树的锯齿形层次遍历;
+
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+class Solution2 {
+
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        travel(root, res, 0);
+        return res;
+    }
+
+    private void travel(TreeNode cur, List<List<Integer>> res, int level) {
+        if (cur == null) {
+            return;
+        }
+
+        //如果res.size() <= level说明下一层的集合还没创建，所以要先创建下一层的集合
+        if (res.size() <= level) {
+            List<Integer> newLevel = new LinkedList<>();
+            res.add(newLevel);
+        }
+
+        //遍历到第几层我们就操作第几层的数据
+        List<Integer> list = res.get(level);
+        //这里默认根节点是第0层，偶数层相当于从左往右遍历，
+        // 所以要添加到集合的末尾，如果是奇数层相当于从右往左遍历，
+        // 要把数据添加到集合的开头
+        if (level % 2 == 0) {
+            list.add(cur.val);
+        } else {
+            list.add(0, cur.val);
+        }
+
+        //分别遍历左右两个子节点，到下一层了，所以层数要加1
+        travel(cur.left, res, level + 1);
+        travel(cur.right, res, level + 1);
+    }
+
+}
