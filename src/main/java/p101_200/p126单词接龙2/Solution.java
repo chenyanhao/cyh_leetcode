567760bd3b88c8abbceb9b34c954a510cd6c49cb
yanhao.cyh <yanhao.cyh@antfin.com>
2020-09-22 15:03:21 +0800

    init p126/127

---
+++ src/main/java/p101_200/p126单词接龙2/Solution.java
@@ -0,0 +1,9 @@
+package p101_200.p126单词接龙2;
+
+import java.util.List;
+
+class Solution {
+    public List<List<String>> findLadders(String beginWord, String endWord, List<String> wordList) {
+        return null;
+    }
+}
---
+++ src/main/java/p101_200/p127单词接龙/Solution.java
@@ -0,0 +1,9 @@
+package p101_200.p127单词接龙;
+
+import java.util.List;
+
+class Solution {
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        return -1;
+    }
+}
