571e713b6ffa9caa4c7d3b189831f0e1da3a26a7
yuanjiuchen <yuanjiuchen@tencent.com>
2022-03-07 16:47:31 +0800

    update to 35

--- src/main/java/p301_400/p494目标和/Solution2.java
+++ src/main/java/p301_400/p494目标和/Solution2.java
@@ -2,40 +2,37 @@ package p301_400.p494目标和;
 
 import java.util.*;
 
+/**
+ * 通过备忘录优化。
+ * 可以看到备忘录 cache 目前是个 HashMap，
+ * 其实也可以是个二维数组，一维是 depth，另一维是 trackSum。
+ * 有次可见，带备忘录优化的递归和DP是等价的。
+ */
 class Solution2 {
 
-    private Map<String, Boolean> cache = new LinkedHashMap<>();
-
     public int findTargetSumWays(int[] nums, int target) {
-        List<List<Character>> ans = new LinkedList<>();
-        dfs(nums, 0, 0, target, ans, new LinkedList<>());
-        return ans.size();
+        return dfs(nums, 0, 0, target, new HashMap<>());
     }
 
     // 备忘录 cache 也可以是个二维数组，一维是 depth，另一维是 trackSum
-    private void dfs(int[] nums, int depth, int trackSum, int target,
-                     List<List<Character>> ans, LinkedList<Character> path) {
+    private int dfs(int[] nums, int depth, int trackSum, int target, Map<String, Integer> cache) {
         if (depth == nums.length) {
             if (trackSum == target) {
-                ans.add(new LinkedList<>(path));
+                return 1;
             }
-            return;
+            return 0;
         }
 
         String key = depth + "_" + trackSum;
         if (cache.containsKey(key)) {
-            return;
+            return cache.get(key);
         }
 
-        path.addLast('+');
-        dfs(nums, depth+1, trackSum+nums[depth], target, ans, path);
-        path.removeLast();
+        int left = dfs(nums, depth+1, trackSum+nums[depth], target, cache);
+        int right = dfs(nums, depth+1, trackSum-nums[depth], target, cache);
 
-        path.addLast('-');
-        dfs(nums, depth+1, trackSum-nums[depth], target, ans, path);
-        path.removeLast();
-
-        cache.put(key, true);
+        cache.put(key, left + right);
+        return left + right;
     }
 
     public static void main(String[] args) {
